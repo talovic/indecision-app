ed8787a9417a5fa38ff75717256dff742c5136dc
Talovic <dzevad88@gmail.com>
2017-11-29 09:12:57 +0100

    Styling react modal

--- src/components/OptionModal.js
+++ src/components/OptionModal.js
@@ -7,10 +7,16 @@ const OptionModal = props => {
       isOpen={!!props.selectedOption}
       onRequestClose={props.handleClearSelectedOption}
       constentLabel="Selected Option"
+      closeTimeoutMS={200}
+      className="modal"
     >
-      <h3>Selected Option</h3>
-      {props.selectedOption && <p>{props.selectedOption}</p>}
-      <button onClick={props.handleClearSelectedOption}>Okey</button>
+      <h3 className="modal__title">Selected Option</h3>
+      {props.selectedOption && (
+        <p className="modal__body">{props.selectedOption}</p>
+      )}
+      <button className="button" onClick={props.handleClearSelectedOption}>
+        Okey
+      </button>
     </Modal>
   );
 };
